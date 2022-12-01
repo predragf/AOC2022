be400bba60d7a0c71028136b13dc17cca6f79ec4
predragf <predragku@gmail.com>
2022-12-01 21:32:20 +0100

    Day 1

---
+++ Day 1/solution.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+from time import perf_counter_ns
+
+
+def problem_1(_input):
+    return max([sum([int(y) for y in x.split()]) for x in _input.split("\n\n")])
+    
+
+def problem_2(_input):
+    return sum(sorted([sum([int(y) for y in x.split()]) for x in _input.split("\n\n")], reverse=True)[0:3])
+
+
+def main():
+    with open("input.txt") as i_file:
+        _input = i_file.read()        
+    
+    p1 = perf_counter_ns()
+    solution_1 = problem_1(_input)
+    p2 = perf_counter_ns()
+    solution_2 = problem_2(_input)
+    p3 = perf_counter_ns()
+    
+    print(f"solution first part: {solution_1} ({(p2-p1)/1_000_000})")
+    print(f"solution second part: {solution_2} ({(p3-p2)/1_000_000})")
+
+main()
+
---
+++ new_day.sh
@@ -0,0 +1,33 @@
+current_day="Day ${1}"
+
+mkdir ./"${current_day}"
+cd ./"${current_day}"
+echo '#! /usr/bin/python3
+from time import perf_counter_ns
+
+
+def problem_1(_input):
+    pass
+
+
+def problem_2(_input):
+    pass
+
+
+def main():
+
+    with open("input.txt") as i_file:
+        _input = i_file.readlines()    
+    
+    p1 = perf_counter_ns()
+    solution_1 = problem_1(_input)
+    p2 = perf_counter_ns()
+    solution_2 = problem_2(_input)
+    p3 = perf_counter_ns()
+    print(f"solution first part: {solution_1} ({(p2-p1)/1_000_000})")
+    print(f"solution second part: {solution_2} ({(p3-p2)/1_000_000})")
+
+main()
+' > ./solution.py
+touch ./input.txt
+touch ./notes.txt
