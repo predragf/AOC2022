668d4aae6c09366f8d7b4d8731bb1f1b378b0046
predragf <predragku@gmail.com>
2022-12-01 22:03:33 +0100

    project creating script improvements

--- new_day.sh
+++ new_day.sh
@@ -2,7 +2,7 @@ current_day="Day ${1}"
 
 mkdir ./"${current_day}"
 cd ./"${current_day}"
-echo '#! /usr/bin/python3
+echo "#! /usr/bin/python3
 from time import perf_counter_ns
 
 
@@ -16,7 +16,7 @@ def problem_2(_input):
 
 def main():
 
-    with open("input.txt") as i_file:
+    with open(\"input.txt\") as i_file:
         _input = i_file.readlines()    
     
     p1 = perf_counter_ns()
@@ -24,10 +24,11 @@ def main():
     p2 = perf_counter_ns()
     solution_2 = problem_2(_input)
     p3 = perf_counter_ns()
-    print(f"solution first part: {solution_1} ({(p2-p1)/1_000_000})")
-    print(f"solution second part: {solution_2} ({(p3-p2)/1_000_000})")
+    print(\"---------- ${current_day} ----------\")
+    print(f\"solution first part: {solution_1} ({(p2 - p1)/1_000_000} ms)\")
+    print(f\"solution second part: {solution_2} ({(p3 - p2)/1_000_000} ms)\")
 
 main()
-' > ./solution.py
+" > ./solution.py
 touch ./input.txt
 touch ./notes.txt
