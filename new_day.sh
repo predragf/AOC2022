6d2f0c96f70632e383d2e5fe594d2664635a3f2d
predragf <predragku@gmail.com>
2022-12-02 22:40:15 +0100

    changes in the project template

--- new_day.sh
+++ new_day.sh
@@ -6,27 +6,26 @@ echo "#! /usr/bin/python3
 from time import perf_counter_ns
 
 
-def problem_1(_input):
+def part_one(_input):
     pass
 
 
-def problem_2(_input):
+def part_two(_input):
     pass
 
 
 def main():
-
     with open(\"input.txt\") as i_file:
         _input = i_file.readlines()    
     
     p1 = perf_counter_ns()
-    solution_1 = problem_1(_input)
+    solution_part_one = part_one(_input)
     p2 = perf_counter_ns()
-    solution_2 = problem_2(_input)
+    solution_part_two = part_two(_input)
     p3 = perf_counter_ns()
     print(\"---------- ${current_day} ----------\")
-    print(f\"solution first part: {solution_1} ({(p2 - p1)/1_000_000} ms)\")
-    print(f\"solution second part: {solution_2} ({(p3 - p2)/1_000_000} ms)\")
+    print(f\"solution first part: {solution_part_one} ({(p2 - p1) / 1_000_000} ms)\")
+    print(f\"solution second part: {solution_part_two} ({(p3 - p2) / 1_000_000} ms)\")
 
 main()
 " > ./solution.py
